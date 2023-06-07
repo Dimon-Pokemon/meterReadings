64e57bce73be21e1e593fb7bdf574d11c01abb40
Dimon-Pokemon <purumamuma@gmail.com>
2023-06-07 21:22:46 +0400

    Написал класс MeteringDevice. Он представляет прибор учета. Экземпляры этого класса по идее будут отображаться на ui

--- src/main/java/model/MeteringDevice.java
+++ src/main/java/model/MeteringDevice.java
@@ -1,11 +1,61 @@
 package model;
 
-public class MeteringDevice {
-    MeteringDevice(){
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
+public class MeteringDevice {
+    private final StringProperty street;
+    private final StringProperty typeMeteringDevice;
+
+    private final LongProperty serialNumber;
+
+
+    MeteringDevice(String street, String typeMeteringDevice, Long serialNumber){
+        this.street = new SimpleStringProperty(street);
+        this.typeMeteringDevice = new SimpleStringProperty(typeMeteringDevice);
+        this.serialNumber = new SimpleLongProperty(serialNumber);
     }
 
-    MeteringDevice(){
+    MeteringDevice(Street street, TypeMeteringDevice typeMeteringDevice, Long serialNumber){
+        this(street.toString(), typeMeteringDevice.toString(), serialNumber);
+    }
 
+
+    public long getSerialNumber() {
+        return serialNumber.get();
+    }
+
+    public LongProperty getSerialNumberProperty() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(Long newSerialNumber){
+        this.serialNumber.set(newSerialNumber);
+    }
+
+    public String getTypeMeteringDevice() {
+        return typeMeteringDevice.get();
+    }
+
+    public StringProperty getTypeMeteringDeviceProperty() {
+        return typeMeteringDevice;
+    }
+
+    public void setTypeMeteringDevice(String newTypeMeteringDevice){
+        this.typeMeteringDevice.set(newTypeMeteringDevice);
+    }
+
+    public String getStreet() {
+        return street.get();
+    }
+
+    public StringProperty getStreetProperty() {
+        return street;
+    }
+
+    public void setStreet(String newStreet){
+        this.street.set(newStreet);
     }
 }
--- src/main/java/model/TypeMeteringDevice.java
+++ src/main/java/model/TypeMeteringDevice.java
@@ -7,48 +7,5 @@ import javafx.beans.property.LongProperty;
 
 public class TypeMeteringDevice {
 
-    private final StringProperty street;
-    private final StringProperty typeMeteringDevice;
 
-    private final LongProperty serialNumber;
-
-
-    TypeMeteringDevice(String street, String typeMeteringDevice, Long serialNumber){
-        this.street = new SimpleStringProperty(street);
-        this.typeMeteringDevice = new SimpleStringProperty(typeMeteringDevice);
-        this.serialNumber = new SimpleLongProperty(serialNumber);
-    }
-
-    TypeMeteringDevice(Street street, TypeMeteringDevice typeMeteringDevice, Long serialNumber){
-        this(street.toString(), typeMeteringDevice.toString(), serialNumber);
-    }
-
-
-    public long getSerialNumber() {
-        return serialNumber.get();
-    }
-
-    public LongProperty serialNumberProperty() {
-        return serialNumber;
-    }
-
-    public void setSerialNumberProperty(Long serialNumber){
-        this.serialNumber.set(serialNumber);
-    }
-
-    public String getTypeMeteringDevice() {
-        return typeMeteringDevice.get();
-    }
-
-    public StringProperty typeMeteringDeviceProperty() {
-        return typeMeteringDevice;
-    }
-
-    public String getStreet() {
-        return street.get();
-    }
-
-    public StringProperty streetProperty() {
-        return street;
-    }
 }
